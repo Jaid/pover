5a6312e3f07ea53e2c97316e9d1454f231928106
Jaid <jaid.jsx@gmail.com>
2019-02-15 01:06:21 +0100

    Fixed test
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/index.test.js
+++ test/index.test.js
@@ -5,6 +5,7 @@ import coffee from "coffee"
 const main = path.resolve(process.env.MAIN)
 console.log(main)
 
-it("should run with 1 argument", () => coffee.fork(main)
+it("should run with 1 argument", () => coffee.fork(main, ["debug"])
   .expect("code", 0)
+  .debug()
   .end())
