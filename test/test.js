1ccf8601f5615dcf036dc78528e136b7d9f4b78c
Jaid <jaid.jsx@gmail.com>
2019-02-21 01:17:32 +0100

    Fixed scripts and removed coverage report
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/test.js
+++ test/test.js
@@ -3,7 +3,6 @@ import path from "path"
 import coffee from "coffee"
 
 const main = path.resolve(process.env.MAIN)
-console.log(main)
 
 it("should run internal command", () => coffee.fork(main, ["debug"])
   .expect("code", 0)
